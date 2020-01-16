f92bac2e95fbd99e802f3aa645fbab9b80a0b6da
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 18:53:26 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,15 @@
-print('hello')
-print('world')
-print('world')
+def increment(time, seconds):
+    time.second += seconds
     
+    if time.second >= 60:
+        time.second -= 60
+        time.minute += 1
+ 
+    if time.minute >= 60:
+        time.minute -=60
+        time.hour += 1
+        
+ def time_to_int(time):
+     minutes = time.hour * 60 + time.minutes
+     seconds = minutes * 60 + time.second
+     return seconds
